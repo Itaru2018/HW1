ddb7a68b084eb3b66ff5ca13ed2ac64fc6b843d0
theeg <the.eggs.piero@gmail.com>
2024-12-08 19:15:47 +0100

    put the question on the file 5

---
+++ Questions_Numbers/Question_Number_5.py
@@ -0,0 +1,4 @@
+#Question 5 ****************************************************************************
+#num = 123456
+#Using the // (floor division) and % (modolus) operators print out the digits in reverse order
+#Output should be 654321
