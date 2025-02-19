70c38f06df3c0a915335f886f4cfd9187253e451
Itaru <the.eggs.piero@gmail.com>
2025-02-19 17:39:44 +0100

    If_ sort finished

--- If_sort/If_sort.py
+++ If_sort/If_sort.py
@@ -14,3 +14,27 @@
 # print(a, b, c)
 # When run the output should be:
 # 10, 20, 30
+
+a = 30
+b = 20
+c = 10
+print(a, b, c)
+
+if a > b:
+    temp = a 
+    a = b
+    b = temp
+
+if b > c:
+    # Question about global scope and local scope
+    temp = b
+    b = c
+    c = temp
+
+# Is this correct?
+
+temp = a
+a = b
+b = temp
+       
+print(a, b, c)
